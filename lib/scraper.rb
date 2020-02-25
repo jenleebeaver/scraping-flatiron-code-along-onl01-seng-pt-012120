861d6d11883ccd76dce394aadb36b24dcbe007ad
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-25 13:44:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,7 +26,7 @@ class Scraper
     self.get_page.css(".post")
   end
 
-  def make_courses #operating on collection returned by get_courses 
+  def make_courses #operating on collection returned by get_courses
       self.get_courses.each do |post|
         course = Course.new
         course.title = post.css("h2").text
